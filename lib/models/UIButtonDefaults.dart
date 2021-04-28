c41cd69a149ee15f6737b02c38f6825ede3b1eb4
Felipe Nobre <nobrefelipe1@gmail.com>
2021-04-28 20:50:48 +0200

    create Ui Button Defaults

---
+++ lib/models/UIButtonDefaults.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+class UIButtonDefaults {
+  Color borderColor;
+  double borderRadius;
+  double borderWidth;
+  double elevation;
+  Color labelColor;
+  Color bgColor;
+  double widthFactor;
+
+  UIButtonDefaults({
+    this.borderColor,
+    this.borderRadius,
+    this.borderWidth,
+    this.elevation,
+    this.labelColor,
+    this.bgColor,
+    this.widthFactor,
+  });
+}
