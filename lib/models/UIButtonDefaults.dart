840220bc0a515692764745dc3686e76ec985670a
Felipe Nobre <nobrefelipe1@gmail.com>
2021-05-04 20:47:04 +0200

    add documentation comments

--- lib/models/UIButtonDefaults.dart
+++ lib/models/UIButtonDefaults.dart
@@ -3,12 +3,32 @@ import 'dart:ui';
 import 'UIDefaults.dart';
 
 class UIButtonDefaults extends UIDefaults {
+  ///
+  /// Sets the button's border color
   Color borderColor;
+
+  ///
+  /// Sets the button's border radius
   double borderRadius;
+
+  ///
+  /// Sets the button's border width
   double borderWidth;
+
+  ///
+  /// Sets the button's elevation
   double elevation;
+
+  ///
+  /// Sets the button's label color
   Color labelColor;
+
+  ///
+  /// Sets the button's background color
   Color bgColor;
+
+  ///
+  /// Sets the button's size percentage
   double widthFactor;
 
   UIButtonDefaults({
--- lib/models/UITextFieldDefaults.dart
+++ lib/models/UITextFieldDefaults.dart
@@ -4,15 +4,44 @@ import '../flutter_ui_toolkit.dart';
 import 'UIDefaults.dart';
 
 class UITextFieldDefaults extends UIDefaults {
+  ///
+  /// Set the input's border radius
   final double borderRadius;
+
+  ///
+  /// Set the input's border type [UIBorderType]
   final UIBorderType borderType;
+
+  ///
+  /// Set the input's border color
   final Color borderColor;
+
+  ///
+  /// Set the input's FOCUS border color
   final Color focusBorderColor;
+
+  ///
+  /// Set the input's ERROR border color
   final Color errorBorderColor;
+
+  ///
+  /// Set the input's border width
   final double borderWidth;
+
+  ///
+  /// Set the input's hint color
   final Color hintColor;
+
+  ///
+  /// Set the input's inner padding
   final double padding;
+
+  ///
+  /// Set the input's background color
   final Color bgColor;
+
+  ///
+  /// Set the input's label color
   final Color labelColor;
 
   UITextFieldDefaults({
